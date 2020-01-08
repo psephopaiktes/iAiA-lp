bee7815d609a6cb4f03c1e52cbc8b78c34f30272
a-hirata <hirataakira6@gmail.com>
2020-01-08 23:51:13 +0900

    fix LP

--- src/js/index.ts
+++ src/js/index.ts
@@ -14,3 +14,39 @@ window.console.log(
     padding: 20px 0 40px;
   `
 );
+
+// Keyart Fadein
+window.addEventListener("load", () => {
+  document.getElementById("js-keyart").classList.add("show");
+});
+
+// Show CTA when scroll
+const heroCta = document.getElementById("js-hero-cta");
+const fixCta = document.getElementById("js-fix-cta");
+const ctaObserver = new IntersectionObserver(entries => {
+  if (entries[0].isIntersecting) {
+    fixCta.classList.remove("show");
+  } else {
+    fixCta.classList.add("show");
+  }
+});
+ctaObserver.observe(heroCta);
+
+// Scroll reveal effect
+const options = {
+  root: null,
+  rootMargin: "0px 0px -10%",
+  threshold: 0
+};
+const revealObserver = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+}, options);
+document.querySelectorAll(".js-reveal").forEach(elm => {
+  revealObserver.observe(elm);
+});
